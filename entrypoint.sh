ae85d5b6aa13eee8fb08c91ac6211f890a2a3ae2
jeff51419 <jeff51419@gmail.com>
2021-09-03 11:25:18 +0800

    init

---
+++ entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+set -e
+set -x
+
+if [ -z "$INPUT_SOURCE_FILE" ]
+then
+  echo "Source file must be defined"
+  return -1
+fi
+
+if [ -z "$INPUT_DESTINATION_BRANCH" ]
+then
+  INPUT_DESTINATION_BRANCH=main
+fi
+OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
+
+CLONE_DIR=$(mktemp -d)
+
+echo "----------------------------------"
+echo "Cloning destination git repository"
+git config --global user.email "$INPUT_USER_EMAIL"
+git config --global user.name "$INPUT_USER_NAME"
+git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+
+echo "----------------------------------"
+echo "Copying contents to git repo"
+mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
+pwd
+cp -R "$INPUT_SOURCE_FILE" "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+cd "$CLONE_DIR"
+
+if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
+then
+  git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
+  OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH_CREATE"
+fi
+
+if [ -z "$INPUT_COMMIT_MESSAGE" ]
+then
+  INPUT_COMMIT_MESSAGE="Update from https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}"
+fi
+
+echo "----------------------------------"
+echo "Adding git commit"
+git add .
+echo "----------------------------------"
+if git status | grep -q "Changes to be committed"
+then
+  git commit --message "$INPUT_COMMIT_MESSAGE"
+  echo "Pushing git commit"
+  git push -u origin HEAD:$OUTPUT_BRANCH
+else
+  echo "No changes detected"
+fi
